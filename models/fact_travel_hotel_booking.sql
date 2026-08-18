412ae42a12a8d8de49c2028a38cf53ea59ea0890
EUGENIE DIMIER <eugenie_dimier@outlook.fr>
2026-08-18 16:00:56 +0000

    v2

---
+++ models/fact_travel_hotel_booking.sql
@@ -0,0 +1,24 @@
+
+
+CREATE OR REPLACE TABLE oceanic-monitor-493207-i6.dbt_ellana94.fact_travel_hotel_booking AS (
+
+SELECT
+    rooms.booking_reference,
+    rooms.booked_at,
+    rooms.start_date,
+    rooms.end_date,
+    rooms.booking_status,
+    rooms.room_fee AS booking_price,
+    hotels.commission_percentage,
+    rooms.organisation_id,
+    rooms.organisation_name,
+    rooms.user_id,
+    rooms.hotel_id,
+    hotels.name AS hotel_name,
+    rooms.id AS hotel_room_id,
+    rooms.payment_done_at
+FROM carteldeladata.modelisation_sources.rooms AS rooms
+LEFT JOIN carteldeladata.modelisation_sources.hotels AS hotels
+    ON hotels.id = rooms.hotel_id
+
+)
